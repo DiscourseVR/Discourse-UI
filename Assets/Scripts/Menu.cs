27b9ad086b1d0e9af6ba86d8e7a12170954bc816
Zhehai <zhehaizhang1@gmail.com>
2021-04-10 12:05:08 -0400

    logic

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,14 +1,35 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class Menu : MonoBehaviour
 {
+    // Define the Objects
+    public GameObject start,view;
+
+    public GameObject debate,menu;
+    private Button btnStart, btnView;
     // Start is called before the first frame update
     void Start()
     {
-        
+      btnStart = start.GetComponent<Button>();
+      btnView = view.GetComponent<Button>();
+      btnStart.onClick.AddListener(onStart);
+      btnView.onClick.AddListener(onView);
     }
+
+    void onStart()
+    {
+      Debug.Log("Clicked start");
+      debate.SetActive(true);
+      menu.SetActive(false);
+    }
+    void onView()
+    {
+      Debug.Log("Clicked view");
+    }
+
 
     // Update is called once per frame
     void Update()
--- Assets/Scripts/gameLogic.cs
+++ Assets/Scripts/gameLogic.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class gameLogic : MonoBehaviour
 {
+    // Define the Objects
+    public GameObject start_timer, button, message;
+
+    private TextMeshPro Timer, Message;
+
+    private int time = 0;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+      Timer = start_timer.GetComponent<TextMeshPro>();
+      Message = message.GetComponent<TextMeshPro>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        getStatus();
+        getTimer();
+    }
+
+    void getTimer()
+    {
+      time+=1;
+      Timer.SetText("{0}:{1}",time%60,time);
+    }
+    void getStatus()
+    {
+      Message.SetText("message set");
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+      Debug.Log(collision.gameObject.name);
+      if (collision.gameObject.name == "hand") {
+        Debug.Log("hand touched");
+      }
     }
 }
